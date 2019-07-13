7891d104238bb5fb344cad3f4ee85c43b914755f
Felipe <felipesantosteixeira@outlook.com>
2019-07-13 16:24:07 -0300

    checkout

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -21,7 +21,8 @@ const TESTE = [
 @Component({
     selector: "ns-items",
     moduleId: module.id,
-    templateUrl: "./items.component.html"
+    templateUrl: "./items.component.html",
+    styleUrls: ['./items.component.css'],
 })
 export class ItemsComponent implements OnInit {
 
