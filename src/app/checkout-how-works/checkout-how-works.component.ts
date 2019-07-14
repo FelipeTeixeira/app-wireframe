1e58e919584a79a1bda4da80d93ac26c28f24bf5
Felipe <felipesantosteixeira@outlook.com>
2019-07-14 20:32:16 -0300

    page how works

---
+++ src/app/checkout-how-works/checkout-how-works.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-checkout-how-works',
+  templateUrl: './checkout-how-works.component.html',
+  styleUrls: ['./checkout-how-works.component.css'],
+  moduleId: module.id,
+})
+export class CheckoutHowWorksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
