29efc5f204435b6c85a41771347376005430ceaa
Felipe <felipesantosteixeira@outlook.com>
2019-07-21 23:29:46 -0300

    step 3

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,13 +1,17 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule } from 'nativescript-angular/common';
 import { SignupComponent } from './signup/signup.component';
+import { NativeScriptLocalizeModule } from "nativescript-localize/angular";
+import { IconPasswordMustComponent } from '../icon-password-must/icon-password-must.component';
 
 @NgModule({
     declarations: [
-        SignupComponent
+        SignupComponent,
+        IconPasswordMustComponent
     ],
     imports: [
-        NativeScriptCommonModule
+        NativeScriptCommonModule,
+        NativeScriptLocalizeModule
     ],
     schemas: [NO_ERRORS_SCHEMA],
     exports: [
